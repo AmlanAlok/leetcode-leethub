bef3092bde6cc76f09a0b83c26511a16b0c189ba
Amlan Alok <amlanalok@gmail.com>
2023-03-04 21:27:23 -0600

    Time: 49 ms (96.10%), Space: 13.9 MB (49.20%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        return p1(x)
+    
+def ans1(num):
+    s = str(num)
+    return s == s[::-1]
+
+def p1(num) -> bool:
+    
+    s = str(num)
+    return s == s[::-1]
+    
