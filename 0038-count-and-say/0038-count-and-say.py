d598a9ab58733cf232ef9a2f31977391fd626344
Amlan Alok <amlanalok@gmail.com>
2023-03-09 23:46:13 -0600

    Time: 56 ms (28.33%), Space: 13.9 MB (46.36%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.py
+++ 0038-count-and-say/0038-count-and-say.py
@@ -1,7 +1,8 @@
 class Solution:
     def countAndSay(self, n: int) -> str:
-        return self.ans1(n)
+        return self.ans2(n)
     
+    '''3 loops'''
     def ans1(self, n: int) -> str:
         
         cur = '1'
@@ -28,8 +29,30 @@ class Solution:
             cur = t
             
         return cur
-                
-                    
+    
+    '''Recursion with 2 loops'''
+    def ans2(self, n: int) -> str:
+        
+        if n == 1:
+            return '1'
+        
+        x = self.ans2(n-1)
+        
+        i, j = 0, 0
+        t = ''
+        while i < len(x):
+            count = 0
+            while j < len(x):
+                if x[i] == x[j]:
+                    count += 1
+                    j += 1
+                else:
+                    break
+            t += str(count) + x[i]
+            i = j
+        
+        return t
+        
     
     
                 
