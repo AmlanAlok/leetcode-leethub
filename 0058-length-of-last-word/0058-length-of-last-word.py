279bf30f4861acb95bcfbfc42f25235b149b2ce8
Amlan Alok <amlanalok@gmail.com>
2023-03-10 23:27:05 -0600

    Time: 35 ms (39.92%), Space: 13.9 MB (28.97%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -1,7 +1,7 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
         
-        return self.ans1(s)
+        return self.ans2(s)
     
     def ans1(self, s: str) -> int:
         
@@ -23,4 +23,17 @@ class Solution:
         
         return count
                 
+    def ans2(self, s: str) -> int:
         
+        i = len(s)-1
+        count = 0
+        
+        while s[i] == ' ' and i >= 0:
+            i -= 1
+        
+        while i >= 0 and s[i] is not ' ':
+            count += 1
+            i -= 1
+        
+        return count
+    
