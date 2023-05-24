461cd572065be14565ca1e9b7cf726aad28bd98b
Amlan Alok <amlanalok@gmail.com>
2023-05-23 22:48:06 -0500

    Time: 57 ms (21.10%), Space: 16.6 MB (5.36%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -0,0 +1,23 @@
+'''
+[1,2,2,1,1,3]
+[1,2]
+[-3,0,1,-3,1,1,1,-3,10,0]
+'''
+
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        
+        d = {}
+        for n in arr:
+            d[n] = d[n] + 1 if n in d else 1
+        
+        vd = {}
+        
+        for v in list(d.values()):
+            if v in vd:
+                return False
+            else:
+                vd[v] = 1
+                
+        return True
+                
