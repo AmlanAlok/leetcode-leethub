d6c5db1b93d52afc82289052fe53c63e4d4085f3
Amlan Alok <amlanalok@gmail.com>
2023-05-25 22:52:21 -0500

    Time: 39 ms (33.24%), Space: 16.7 MB (6.66%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.py
@@ -0,0 +1,85 @@
+'''
+Test case
+"/home/"
+"/../"
+"/home//foo/"
+"/a/./b/../../c/"
+'''
+class Solution:
+    
+    def simplifyPath(self, path: str) -> str:
+        return ans2(path)
+        
+def fail1(path: str) -> str:
+        
+        s = path
+        
+        s = s.replace('//','/')
+        s = s.replace('.','')
+        
+        if s[0] != '/':
+            s = '/'+s
+        
+        if s[-1] == '/':
+            s = s[:-1]
+            
+        return s
+        
+def ans1(path: str) -> str:
+        
+        from collections import deque
+        
+        s = deque()
+        p = path
+        
+        p = p.split('/')
+        
+        print(p)
+        
+        for i, v in enumerate(p):
+
+            if v == '..' and s:
+                s.pop()
+                
+            elif v == '.' or v == '' or v == '..':
+                continue
+            
+            elif isinstance(v, str):
+                s.append(v)
+            
+        print(s)
+                
+        return '/'+'/'.join(s)
+            
+def ans2(path):
+    from collections import deque
+        
+    path = path.replace('//', '/')
+    parts = path.split('/')
+    
+    s = deque()
+    
+    for p in parts:
+        if p == '..':
+            if len(s) > 0:
+                s.pop()
+            continue
+        if p == '.':
+            continue
+        if p is not '':
+            s.append(p)
+    
+    ans = ''
+    
+    for st in s:
+        ans += '/' + st
+    
+    if ans is '':
+        return '/'
+    return ans
+    
+    
+            
+            
+    
+        
