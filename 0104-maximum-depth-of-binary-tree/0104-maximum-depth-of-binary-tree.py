7f4ff0cefffc7d72436ece0549bdb846b1bf4506
Amlan Alok <amlanalok@gmail.com>
2023-07-04 22:24:40 -0500

    Time: 58 ms (54.40%), Space: 18.6 MB (63.84%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -39,6 +39,13 @@ def ans1(root):
         return max_depth(root)
     return 0
     
+'''
+TC = n
+SC = 
+worst case, the tree is completely unbalanced, e.g. each node has only left child node, the recursion call would occur N times (the height of the tree), therefore the storage to keep the call stack would be O(N)
+
+best case (the tree is completely balanced), the height of the tree would be log(N)
+'''
 def ans2(root):
     
     if root == None:
