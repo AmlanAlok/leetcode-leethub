895334083b4905b5761da5706da1fb3c9ee46139
Amlan Alok <amlanalok@gmail.com>
2023-08-12 18:06:12 -0500

    Time: 38 ms (88.53%), Space: 16.3 MB (34.63%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -12,24 +12,23 @@ class Solution:
 '''TC = n, SC = n'''
 def ans1(stones):
     
-    nums = [i*-1 for i in stones]
+    nums = [-i for i in stones]
     heapify(nums)
-    # heapq._heapify_max(stones)        # allows you to create a max heap but I do not know it corresponding functions yet
+    # heapq._heapify_max(stones)        # allows you to create a max heap but I do not know its corresponding functions yet
     # print(nums)
-    
 
     while len(nums) >= 2:
-        # y, x = nums[0]*-1, nums[1]*-1           # fails here [7,5,8]
+        # y, x = nums[0]*-1, nums[1]*-1           # fails here [7,5,8], only maintains min at first index
         
-        y = heappop(nums) * -1
-        x = heappop(nums) * -1
+        y = -heappop(nums)
+        x = -heappop(nums)
         
         if x != y:
             y = y-x
-            heappush(nums, y*-1)
+            heappush(nums, -y)
     
         # print(nums)
     
-    return nums[0]*-1 if len(nums) == 1 else 0
+    return -nums[0] if len(nums) == 1 else 0
     
     
