4ecc8341b71889278f1b007232bb280535af7ad6
Amlan Alok <amlanalok@gmail.com>
2023-08-14 22:28:34 -0500

    Time: 27 ms (99.59%), Space: 16.9 MB (5.16%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -7,10 +7,12 @@
 "ahbgdc"
 ""
 ""
+"acb"
+"ahbgdc"
 '''
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        return ans1(s, t)
+        return ans2(s, t)
 
 def ans1(s, t):
     
@@ -28,4 +30,47 @@ def ans1(s, t):
         j+=1
     
     return False
+
+
+def ans2(s, t):
+    
+    d = {}
+    
+    for i in range(len(t)):
+        v = t[i]
+        
+        if v in d:
+            d[v].append(i)
+        else:
+            d[v] = [i]
+    print(d)
+    t = -1
+    
+    for c in s:
+        
+        if c in d:
+            idx = d[c]
+            
+            '''Using linear search to find appropiate index'''
+            
+            if idx[-1] > t:
+                for n in idx:
+                    if n > t:
+                        t = n
+                        break
+            else:
+                return False        # if there are no idx values that are larger than t
+            
+        else:
+            return False
+    
+    return True
+            
+        
+            
+    
+    
+    
+    
+    
         
