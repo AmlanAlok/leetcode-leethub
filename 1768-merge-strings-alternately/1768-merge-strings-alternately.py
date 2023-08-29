2614d5a824d18e657d0a4f20f9e8bb1d7e81238d
Amlan Alok <amlanalok@gmail.com>
2023-08-29 14:49:55 -0500

    Time: 46 ms (33.88%), Space: 16.3 MB (70.13%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -0,0 +1,24 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        return ans1(word1, word2)
+    
+
+def ans1(word1, word2):
+    
+    ans = ''
+    i,j = 0, 0
+    
+    while i < len(word1) or j < len(word2):
+        
+        if i < len(word1):
+            ans += word1[i]
+            i += 1
+        if j < len(word2):
+            ans += word2[j]
+            j += 1
+            
+    return ans
+            
+        
+        
+        
