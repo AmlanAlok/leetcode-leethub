3276c058001d5e9e83f25da8cb55c837c97047ff
Amlan Alok <amlanalok@gmail.com>
2023-09-27 22:13:49 -0500

    Time: 41 ms (79.48%), Space: 16.9 MB (97.13%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -1,3 +1,13 @@
+from collections import deque
+
+'''
+Level Order Traversal means BFS (Breadth First Search). BFS is done using queue.
+'''
+'''
+[3,9,20,null,null,15,7]
+[1]
+[]
+'''
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -6,7 +16,7 @@
 #         self.right = right
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        return ans2(root)
+        return ans1(root)
     
 def max_depth(root):
     
@@ -14,7 +24,8 @@ def max_depth(root):
         return 0
     
     return 1 + max(max_depth(root.left), max_depth(root.right))
-    
+
+'''This is DFS not BFS'''
 def ans2(root, levels=None, idx = 0):
     if levels is None:
         levels = [None]*max_depth(root)
@@ -30,8 +41,8 @@ def ans2(root, levels=None, idx = 0):
     ans2(root.right, levels, idx + 1)
     return levels
 
-    
-def ans1(root):
+'''Does not work'''
+def e1(root):
 
     if root is None:
         return 
@@ -42,3 +53,32 @@ def ans1(root):
     ans1(root.right)
     
     return
+
+def ans1(root):
+    
+    ans = []
+    q = deque()
+    
+    if root:
+        q.append(root)
+        
+        while q:
+
+            x = []
+
+            for i in range(len(q)):
+                node = q.popleft()
+
+                x.append(node.val)
+
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+
+            ans.append(x)
+    
+    return ans
+            
+    
+    
