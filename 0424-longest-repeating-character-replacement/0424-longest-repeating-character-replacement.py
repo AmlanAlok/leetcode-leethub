456582b896060071fdaea5accb9cf04f6671a9e9
Amlan Alok <amlanalok@gmail.com>
2023-10-10 14:15:00 -0500

    Time: 131 ms (35.43%), Space: 16.2 MB (87.60%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -23,4 +23,29 @@ def ans1(s, k):
         r += 1
     
     return res
+
+
+def ans2(s, k):
+    d = {}
+    l = r = 0
+    
+    res = 0
+    maxf = 0
+    
+    # while l < len(s) and r < len(s):
+    for r in range(len(s)):
+        
+        d[s[r]] = d.get(s[r], 0) + 1
+        maxf = max(maxf, d[s[r]])
+        
+        # max(d.values()) is TC = O(26)
+        while (r-l+1) - maxf > k:
+            d[s[l]] -= 1
+            l += 1
+        
+        res = max(res, (r-l+1))
+        
+        r += 1
+    
+    return res
         
