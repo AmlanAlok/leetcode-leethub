44617c8642588970a93b657fb64c4b40b6bbcf44
Amlan Alok <amlanalok@gmail.com>
2023-10-29 18:12:22 -0500

    Time: 417 ms (85.07%), Space: 0B (100.00%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,3 +1,6 @@
+# {"headers":{"Employee":["id","salary"]},"rows":{"Employee":[[1,100]]}}
+# {"headers":{"Employee":["id","salary"]},"rows":{"Employee":[[1,100],[2,200],[3,300]]}}
+# {"headers":{"Employee":["id","salary"]},"rows":{"Employee":[[1,100],[2,100]]}}
 # Write your MySQL query statement below
 
 # select count(*) from employee;
@@ -14,6 +17,18 @@
 # order by salary desc
 # limit 2;
 
-select max(salary) as `SecondHighestSalary`
-from employee
-where salary < (select max(salary) from employee);
+# Ans-1
+# select max(salary) as `SecondHighestSalary`
+# from employee
+# where salary < (select max(salary) from employee);
+
+
+# Ans-2
+select min(salary) as `SecondHighestSalary`
+from (select salary
+    from employee
+    group by salary
+    order by salary desc
+    limit 1
+    offset 1) as x;
+
