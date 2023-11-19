fc9e12e0f47c95420b0034bc0293c552bf3af7de
Amlan Alok <amlanalok@gmail.com>
2023-11-18 19:03:53 -0600

    Time: 715 ms (86.93%), Space: 0B (100.00%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -7,15 +7,16 @@
 # where a.temperature > b.temperature;
 
 -- Ans 2
-select a.id
-from weather a
-join weather b on DATEDIFF(a.recordDate, b.recordDate) = 1
-and a.temperature > b.temperature;
-
 # select a.id
-# from weather a, weather b 
-# where DATEDIFF(a.recordDate, b.recordDate) = 1
+# from weather a
+# join weather b on DATEDIFF(a.recordDate, b.recordDate) = 1
 # and a.temperature > b.temperature;
+
+-- Ans 2
+select a.id
+from weather a, weather b 
+where DATEDIFF(a.recordDate, b.recordDate) = 1
+and a.temperature > b.temperature;
 
 -- Ans 4
 # select a.id as Id
