11fb86618cd798f91aa49c48ad1eb81035f62c2b
Amlan Alok <amlanalok@gmail.com>
2023-11-18 19:02:55 -0600

    Time: 681 ms (90.01%), Space: 0B (100.00%) - LeetHub

---
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -0,0 +1,23 @@
+# Write your MySQL query statement below
+
+-- Ans 1
+select a.id as Id
+from weather a
+join weather b on DATEDIFF(a.recordDate, b.recordDate) = 1
+where a.temperature > b.temperature;
+
+# select a.id
+# from weather a
+# join weather b on DATEDIFF(a.recordDate, b.recordDate) = 1
+# and a.temperature > b.temperature;
+
+# select a.id
+# from weather a, weather b 
+# where DATEDIFF(a.recordDate, b.recordDate) = 1
+# and a.temperature > b.temperature;
+
+-- Ans 4
+# select a.id as Id
+# from weather a, weather b 
+# where a.recordDate - b.recordDate = 1
+# and a.temperature > b.temperature;
