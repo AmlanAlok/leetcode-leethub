85cc489b927a536fe34af413cfb39912415ba2f1
Amlan Alok <amlanalok@gmail.com>
2023-11-22 22:18:50 -0600

    Time: 398 ms (59.13%), Space: 0B (100.00%) - LeetHub

--- 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -7,8 +7,15 @@
 # where a.activity_type = 'end' and b.activity_type = 'start'
 # group by a.machine_id;
 
-select a.machine_id, round(avg(a.timestamp - b.timestamp), 3) as processing_time
+# # ANS-1
+# select a.machine_id, round(avg(a.timestamp - b.timestamp), 3) as processing_time
+# from activity a
+# join activity b on a.machine_id = b.machine_id and a.process_id = b.process_id
+# where a.activity_type = 'end' and b.activity_type = 'start'
+# group by a.machine_id;
+
+# ANS-2
+select a.machine_id, 
+round(sum(case when a.activity_type='start' then a.timestamp*-1 else a.timestamp end)/(select count(distinct a.process_id)), 3) as processing_time
 from activity a
-join activity b on a.machine_id = b.machine_id and a.process_id = b.process_id
-where a.activity_type = 'end' and b.activity_type = 'start'
 group by a.machine_id;
