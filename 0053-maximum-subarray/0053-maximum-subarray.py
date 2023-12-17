bfc36a6bddc09ac9bf2aeb3242fc532a4f750eef
Amlan Alok <amlanalok@gmail.com>
2023-12-17 03:06:04 -0600

    Time: 1772 ms (5.00%), Space: 30.7 MB (8.70%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -9,7 +9,7 @@ import sys
 
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        return dec16(nums)
+        return dec17(nums)
 
 ''' Kadane's Algo, TC = n, SC = 1'''
 def dec16(nums):
@@ -41,6 +41,40 @@ def dec15(nums):
         # best = best if best > t else t
     
     return best
+
+''' Divide & Conquer, TC = n log(n), SC = log(n)'''
+def dec17(nums):
+    
+    def helper(nums, left, right):
+        
+        if left > right:
+            return -sys.maxsize
+        
+        mid = (left + right) // 2
+        
+        mx_left = 0
+        mx_right = 0
+        
+        cur = 0
+        
+        for i in range(mid-1, left-1, -1):
+            cur += nums[i]
+            mx_left = max(mx_left, cur)
+        
+        cur = 0
+        
+        for i in range(mid+1, right+1):
+            cur += nums[i]
+            mx_right = max(mx_right, cur)
+            
+        mx_total = mx_left + nums[mid] + mx_right
+        
+        left_half = helper(nums, left, mid-1)
+        right_half = helper(nums, mid+1, right)
+        
+        return max(mx_total, left_half, right_half)
+    
+    return helper(nums, 0, len(nums)-1)
 
 ''' TC = n, SC = 1 '''
 def ans1(self, nums: List[int]) -> int:
@@ -93,38 +127,5 @@ def p2(self, nums: List[int]) -> int:
 
     return m
 
-''' Divide & Conquer, TC = n log(n), SC = log(n)'''
-def dec17(nums):
-    
-    def helper(nums, left, right):
-        
-        if left > right:
-            return -sys.maxsize
-        
-        mid = (left + right) // 2
-        
-        mx_left = 0
-        mx_right = 0
-        
-        cur = 0
-        
-        for i in range(mid-1, left-1, -1):
-            cur += nums[i]
-            mx_left = max(mx_left, cur)
-        
-        cur = 0
-        
-        for i in range(mid+1, right+1):
-            cur += nums[i]
-            mx_right = max(mx_right, cur)
-            
-        mx_total = mx_left + nums[mid] + mx_right
-        
-        left_half = helper(nums, left, mid-1)
-        right_half = helper(nums, mid+1, right)
-        
-        return max(mx_total, left_half, right_half)
-    
-    return helper(nums, 0, len(nums)-1)
         
         
