f102a073d34e19f1a0aafd5fab40e32d1df167f5
Amlan Alok <amlanalok@gmail.com>
2023-12-18 14:31:26 -0600

    Time: 228 ms (37.32%), Space: 29.5 MB (39.81%) - LeetHub

---
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -0,0 +1,78 @@
+'''
+7
+[2,3,1,2,4,3]
+4
+[1,4,4]
+11
+[1,1,1,1,1,1,1,1]
+11
+[1,2,3,4,5]
+'''
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        return dec18(target, nums)
+    
+''' Two Pointer/ Sliding Window, TC = n, SC = 1 '''
+def ans1(target: int, nums: List[int]) -> int:
+    
+    m = sys.maxsize
+    i = j = s = 0
+    l = len(nums)
+    
+    while j < l:
+        
+        while s < target and j < l:
+            s+=nums[j]
+            j+=1
+        
+        while s >= target and i < l:
+            if m > (j-i):
+                m = j-i
+            s-=nums[i]
+            i+=1
+        
+    return 0 if m == sys.maxsize else m
+
+def ans2(target: int, nums: List[int]) -> int:
+    
+    total = left = right = 0
+    res = len(nums) + 1
+
+    while right < len(nums):
+        total += nums[right]
+        while total >= target:
+            res = min(res, right-left+1)
+            total -= nums[left]
+            left += 1
+        right += 1
+    return res if res <= len(nums) else 0 
+
+def dec18(target, nums):
+    
+    l = len(nums)
+    i = j = 0
+    s = 0
+    mini = sys.maxsize
+    
+    while j < l:
+        
+        while s < target and j < l:
+            s += nums[j]
+            j += 1
+            
+        while s >= target and  i < l:
+            mini = min(mini, j-i)
+            s -= nums[i]
+            i += 1
+            
+    return 0 if mini == sys.maxsize else mini
+    
+        
+        
+    
+    
+        
+        
+
+    
+    
