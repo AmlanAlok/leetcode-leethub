e1348b40033f468eaad1803f8d00490607b062ba
Amlan Alok <amlanalok@gmail.com>
2023-12-22 18:30:28 -0600

    Time: 57 ms (92.27%), Space: 18.6 MB (6.48%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -25,136 +25,46 @@ def dec22(nums, t):
     
     return []
     
-    
-    
-#     def p5(self, nums: List[int], t: int) -> List[int]:
-        
-#         d = {}
-        
-#         for i in range(len(nums)):
-            
-#             v = nums[i]
-#             c = t - v
-            
-#             if c in d and i != d[c]:
-#                 return [d[c], i]
-#             else:
-#                 d[v] = i
-        
+def y22aug01(self, nums: List[int], target: int) -> List[int]:
 
-#     def p4(self, nums: List[int], t: int) -> List[int]:
-        
-#         d = {}
+    dict = {v:i for i,v in enumerate(nums)}
 
-#         for i in range(len(nums)):
-#             v = nums[i]
-
-#             if t-v in d and i is not d[t-v]:
-#                 return [i, d[t-v]]
-#             else:
-#                 d[v] = i
-        
-#         return []
-
-    
-#     def ans_1(self, nums: List[int], target: int) -> List[int]:
-        
-#         dict = {v:i for i,v in enumerate(nums)}
-        
-#         for i in range(len(nums)):
-            
-#             c = target-nums[i]
-#             if c in dict and i is not dict[c]:
-#                 return [i, dict[c]]
-            
-#         return False
-        
-#     def p1(self, nums: List[int], target: int) -> List[int]:
-        
-#         d = {}
-        
-#         for i,v in enumerate(nums):
-            
-#             c= target-v
-            
-#             if c in d and d[c] != i:
-#                 return [i, d[c] ]
-#             else:
-#                 d[v] = i
-        
-#         return []
-            
-#     def p2(self, nums: List[int], target: int) -> List[int]:
-        
-#         d = {}
-        
-#         for i in range(nums):
-            
-#             c= target-v
-            
-#             if c in d and d[c] != i:
-#                 return [i, d[c] ]
-#             else:
-#                 d[v] = i
-        
-#         return []
-    
-    '''
-    TC = n
-    SC = n
-    '''
-    def p3(self, nums: List[int], target: int) -> List[int]:
-        
-        d = {}
-        t = target
-        
-        for i,v in enumerate(nums):
-            c = t-v
-            if c in d and d[c] != i:
-                return [i,d[c]]
-            else:
-                d[v]=i
-        
-        return [-1,-1]
-            
-def p6(nums, target):
-    
-    d = {}
-    
     for i in range(len(nums)):
-        v = nums[i]
-        x = target-v
+
+        c = target-nums[i]
+        if c in dict and i is not dict[c]:
+            return [i, dict[c]]
+
+    return False
         
-        # if x in d and d[x] != i:
-        if x in d:
-            return [d[x], i]
+def y22aug02(self, nums: List[int], target: int) -> List[int]:
+    ''' TC = n, SC = n '''
+    d = {}
+
+    for i,v in enumerate(nums):
+
+        c= target-v
+
+        if c in d and d[c] != i:
+            return [i, d[c] ]
         else:
             d[v] = i
 
-def p7(nums, target):
-    
+    return []
+
+        
+''' != not required'''    
+def old_p5(self, nums: List[int], t: int) -> List[int]:
+
     d = {}
-    
+
     for i in range(len(nums)):
-        n = nums[i]
-        c = target - n
-        
-        if c in d:
-            return [i, d[c]]
+
+        v = nums[i]
+        c = t - v
+
+        if c in d and i != d[c]:
+            return [d[c], i]
         else:
-            d[n] = i
+            d[v] = i
         
-    
-    
-        
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
