68f368e6f629cfdcc2c2f7e51a2cfe1741867da6
Amlan Alok <amlanalok@gmail.com>
2024-03-23 00:13:54 -0500

    Time: 2 ms (97.88%), Space: 45.1 MB (9.76%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,7 +1,7 @@
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        return this.sol1(nums, target);
+        return this.sol(nums, target);
     }
     
     public int[] sol1(int[] nums, int target) {
@@ -18,5 +18,25 @@ class Solution {
         }
         
         return new int[] {};
+    }
+    
+    public int[] sol(int[] nums, int target) {
+        
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for (int i=0; i < nums.length; i++){
+            
+            int n = nums[i];
+            int diff = target - n;
+            
+            if (map.containsKey(diff)){
+                return new int[]{map.get(diff), i};
+            }
+            else {
+                map.put(n, i);
+            }   
+        }
+        
+        return new int[]{};
     }
 }
