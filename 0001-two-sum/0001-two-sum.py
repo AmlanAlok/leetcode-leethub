3159f5a679d91e6aa44bfd861508b29958daa488
Amlan Alok <amlanalok@gmail.com>
2025-04-17 23:20:48 -0500

    Time: 2 ms (53.53%), Space: 19.2 MB (11.51%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -8,96 +8,84 @@
 '''
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        return april28(nums, target)
+        
+        return self.p4(nums, target)
+
+
+    def p4(self, nums: List[int], t: int) -> List[int]:
+        
+        d = {}
+
+        for i in range(len(nums)):
+            v = nums[i]
+
+            if t-v in d and i is not d[t-v]:
+                return [i, d[t-v]]
+            else:
+                d[v] = i
+        
+        return []
+
     
-def dec22(nums, t):
-    d = {}
-    
-    for i in range(len(nums)):
-        n = nums[i]
+    def ans_1(self, nums: List[int], target: int) -> List[int]:
         
-        v = t - n
+        dict = {v:i for i,v in enumerate(nums)}
         
-        if v in d:
-            return [d[v], i]
-        else:
-            d[n] = i
-    
-    return []
-    
-def y22aug01(self, nums: List[int], target: int) -> List[int]:
-
-    dict = {v:i for i,v in enumerate(nums)}
-
-    for i in range(len(nums)):
-
-        c = target-nums[i]
-        if c in dict and i is not dict[c]:
-            return [i, dict[c]]
-
-    return False
-        
-def y22aug02(self, nums: List[int], target: int) -> List[int]:
-    ''' TC = n, SC = n '''
-    d = {}
-
-    for i,v in enumerate(nums):
-
-        c= target-v
-
-        if c in d and d[c] != i:
-            return [i, d[c] ]
-        else:
-            d[v] = i
-
-    return []
-
-
-''' != not required'''    
-def old_p5(self, nums: List[int], t: int) -> List[int]:
-
-    d = {}
-
-    for i in range(len(nums)):
-
-        v = nums[i]
-        c = t - v
-
-        if c in d and i != d[c]:
-            return [d[c], i]
-        else:
-            d[v] = i
-        
-def sol(nums, target):
-    
-    d = {}
-    
-    for i in range(len(nums)):
-        
-        n = nums[i]
-        diff = target - n
-        
-        if diff in d:
-            return [d[diff], i]
-        else:
-            d[n] = i
-        
-    return []
-
-def april28(nums, target):
-    
-    d = {}
-    
-    for i, v in enumerate(nums):
-        
-        complement = target - v
-        
-        if complement in d:
-            return [d[complement], i]
-        else:
-            d[v] = i
+        for i in range(len(nums)):
             
-    return []
+            c = target-nums[i]
+            if c in dict and i is not dict[c]:
+                return [i, dict[c]]
             
+        return False
+        
+    def p1(self, nums: List[int], target: int) -> List[int]:
+        
+        d = {}
+        
+        for i,v in enumerate(nums):
+            
+            c= target-v
+            
+            if c in d and d[c] != i:
+                return [i, d[c] ]
+            else:
+                d[v] = i
+        
+        return []
+            
+    def p2(self, nums: List[int], target: int) -> List[int]:
+        
+        d = {}
+        
+        for i in range(nums):
+            
+            c= target-v
+            
+            if c in d and d[c] != i:
+                return [i, d[c] ]
+            else:
+                d[v] = i
+        
+        return []
     
+    '''
+    TC = n
+    SC = n
+    '''
+    def p3(self, nums: List[int], target: int) -> List[int]:
+        
+        d = {}
+        t = target
+        
+        for i,v in enumerate(nums):
+            c = t-v
+            if c in d and d[c] != i:
+                return [i,d[c]]
+            else:
+                d[v]=i
+        
+        return [-1,-1]
+            
+            
     
